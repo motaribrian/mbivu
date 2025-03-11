e381a4e78e20eb3ab228a96e19b26e4e97118b15
brianmotari <brianmotari02@gmail.com>
2025-03-11 15:58:39 +0300

    Initian commit
    As the Saying mvumilivu hula mbivu this repository is a place for gradual leetcode and data structures and algo brain teasers

---
+++ java_impl/3208. Alternating Groups II/src/main/java/org/codewithmotari/alternatinggroups/Application.java
@@ -0,0 +1,28 @@
+/*
+There is a circle of red and blue tiles. You are given an array of integers colors and an integer k. The color of tile i is represented by colors[i]:
+
+        colors[i] == 0 means that tile i is red.
+        colors[i] == 1 means that tile i is blue.
+
+        An alternating group is every k contiguous tiles in the circle with alternating colors (each tile in the group except the first and last one has a different color from its left and right tiles).
+
+        Return the number of alternating groups.
+
+        Note that since colors represents a circle, the first and the last tiles are considered to be next to each other.
+*/
+
+
+package org.codewithmotari.alternatinggroups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("Yay it ran successfully");
+    }
+
+}
